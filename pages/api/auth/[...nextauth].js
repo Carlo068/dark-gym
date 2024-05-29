10e7a68db84764728e6318a9add48e42d84fcafe
Carlo068 <carlo.ruiz.scch@gmail.com>
2024-05-29 09:11:34 -0600

    Se agrego un generador de mails y la conexion con mongodb

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "@/library/mongodb"
 
 export const authOptions = {
+  adapter: MongoDBAdapter(clientPromise),
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
--- pages/test.js
+++ pages/test.js
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 import Image from 'next/image';
+import toast from 'react-hot-toast';
 
 export default function Test() {
     const [products, setProducts] = useState([]);
@@ -15,8 +16,21 @@ export default function Test() {
         fetchProducts();
     }, []);
 
+    const sendEmail = async () => {
+        const data = {
+            name: 'John Doe',
+            email: 'carlo.ruizo@hotmail.com',
+            message: 'Hello, this is a test email'
+        };
+
+        await axios.post('/api/mail', data)
+        toast.success('Email sent');
+    };
+
     return (
         <div>
+            <input type="email" placeholder="Email" />
+            <button onClick={sendEmail}>Send Email</button>
             {products &&
                 products.length > 0 &&
                 products.map((product) => (
