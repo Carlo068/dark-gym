8fa47654f9a6adba55bbc955f4cd26a8a012d8b9
Carlo068 <carlo.ruiz.scch@gmail.com>
2024-06-08 10:59:30 -0600

    chore: Update npm dependencies and add axios library

--- pages/gym_data.js
+++ pages/gym_data.js
@@ -4,7 +4,7 @@ import { Disclosure } from "@headlessui/react";
 import { ChevronUpIcon, ChevronDownIcon } from "@heroicons/react/solid";
 import { useSession } from "next-auth/react";
 
-import 'tailwindcss/tailwind.css'; // Make sure you have Tailwind CSS configured in your project
+import 'tailwindcss/tailwind.css'; // Asegúrate de tener Tailwind CSS configurado en tu proyecto
 
 export default function GymData() {
     const [workouts, setWorkouts] = useState(null);
@@ -29,7 +29,6 @@ export default function GymData() {
     const handleCheckboxChange = (workoutId) => {
         if (selectedWorkouts.includes(workoutId)) {
             setSelectedWorkouts(selectedWorkouts.filter((id) => id !== workoutId));
-            // Remove the workout details when deselected
             const updatedDetails = { ...workoutDetails };
             delete updatedDetails[workoutId];
             setWorkoutDetails(updatedDetails);
@@ -56,21 +55,18 @@ export default function GymData() {
         const updatedDayWorkouts = { ...dayWorkouts };
         const workoutsToSave = selectedWorkouts.map((workoutId) => ({
             id: workoutId,
-            ...workoutDetails[workoutId] // Include sets and reps
+            ...workoutDetails[workoutId]
         }));
         updatedDayWorkouts[selectedDay] = workoutsToSave;
         setDayWorkouts(updatedDayWorkouts);
 
-         // Send the workout data along with user data to the backend
-         axios.post("/api/save_workout", {
+        axios.post("/api/save_workout", {
             user: session.user._id,
             workout: { [selectedDay]: workoutsToSave }
         }).then(response => {
             console.log("Workout saved successfully:", response.data);
-            // You can handle success here, such as showing a success message to the user
         }).catch(error => {
             console.error("Error saving workout:", error);
-            // You can handle errors here, such as showing an error message to the user
         });
 
         console.log(`Workouts saved for ${selectedDay}:`, updatedDayWorkouts[selectedDay]);
@@ -82,7 +78,7 @@ export default function GymData() {
 
             if (workout) {
                 return (
-                    <div key={workoutId} className="flex items-center justify-between bg-white p-4 rounded shadow mb-2 text-black"> {/* Updated text color */}
+                    <div key={workoutId} className="flex items-center justify-between bg-white p-4 rounded shadow mb-2 text-black">
                         <span className="flex-1">{workout.Ejercicio}</span>
                         <input
                             type="number"
@@ -131,83 +127,97 @@ export default function GymData() {
     };
 
     return (
-        <div className="max-w-7xl mx-auto p-6 bg-gray-100 min-h-screen">
-            <h1 className="text-3xl font-bold text-center mb-8 text-black">Workouts</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {["Chest", "Back", "Triceps", "Biceps", "Legs"].map((category, index) => (
-                    <Disclosure key={category}>
-                        {({ open }) => (
-                            <>
-                                <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-black bg-gray-200 rounded-lg hover:bg-gray-300 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"> {/* Updated text color */}
-                                    <span>{category}</span>
-                                    {open ? (
-                                        <ChevronUpIcon className="w-5 h-5 text-gray-500" />
-                                    ) : (
-                                        <ChevronDownIcon className="w-5 h-5 text-gray-500" />
-                                    )}
-                                </Disclosure.Button>
-                                <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-700">
-                                    <ul className="space-y-2">
-                                        {workouts &&
-                                            workouts.slice(index * 17, (index + 1) * 17).map((workout) => (
-                                                <li key={workout._id} className="flex items-center">
-                                                    <input
-                                                        type="checkbox"
-                                                        id={workout._id}
-                                                        className="mr-2"
-                                                        onChange={() => handleCheckboxChange(workout._id)}
-                                                    />
-                                                    <label htmlFor={workout._id} className="flex-1">{workout.Ejercicio}</label>
-                                                </li>
-                                            ))}
-                                    </ul>
-                                </Disclosure.Panel>
-                            </>
-                        )}
-                    </Disclosure>
-                ))}
-            </div>
-            <div className="mt-8">
-                <h3 className="text-2xl font-semibold mb-4 text-black">Selected Exercises for {selectedDay}:</h3>
-                {getSelectedWorkoutInputs()}
-            </div>
-            <div className="mt-8">
-                <button 
-                    onClick={saveDayWorkout} 
-                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
-                >
-                    Save Workout Plan for {selectedDay}
-                </button>
-            </div>
-            <div className="mt-8">
-                <h3 className="text-2xl font-semibold mb-4 text-black">Select Day of the Week:</h3>
-                <div className="flex space-x-4 text-black">
-                    {["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].map(day => (
-                        <label key={day} className="flex items-center">
-                            <input 
-                                type="radio" 
-                                id={day} 
-                                name="dayOfWeek" 
-                                value={day} 
-                                className="mr-2" 
-                                onChange={handleDayRadiobuttonChange} 
-                                checked={selectedDay === day} 
-                            />
-                            {day}
-                        </label>
+        <div className="flex flex-col min-h-screen bg-gray-100 p-6">
+            {/* Encabezado */}
+            <header className="text-center">
+                <h1 className="text-3xl font-bold text-black mb-8">Workouts</h1>
+            </header>
+
+            {/* Contenido Principal */}
+            <main className="flex flex-col flex-grow items-center">
+                <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {["Chest", "Back", "Triceps", "Biceps", "Legs"].map((category, index) => (
+                        <Disclosure key={category}>
+                            {({ open }) => (
+                                <>
+                                    <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-black bg-gray-200 rounded-lg hover:bg-gray-300 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                                        <span>{category}</span>
+                                        {open ? (
+                                            <ChevronUpIcon className="w-5 h-5 text-gray-500" />
+                                        ) : (
+                                            <ChevronDownIcon className="w-5 h-5 text-gray-500" />
+                                        )}
+                                    </Disclosure.Button>
+                                    <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-700">
+                                        <ul className="space-y-2">
+                                            {workouts &&
+                                                workouts.slice(index * 17, (index + 1) * 17).map((workout) => (
+                                                    <li key={workout._id} className="flex items-center">
+                                                        <input
+                                                            type="checkbox"
+                                                            id={workout._id}
+                                                            className="mr-2"
+                                                            onChange={() => handleCheckboxChange(workout._id)}
+                                                        />
+                                                        <label htmlFor={workout._id} className="flex-1">{workout.Ejercicio}</label>
+                                                    </li>
+                                                ))}
+                                        </ul>
+                                    </Disclosure.Panel>
+                                </>
+                            )}
+                        </Disclosure>
                     ))}
                 </div>
-            </div>
-            <table className="table-auto w-full mt-8">
-                <thead>
-                    <tr className="bg-gray-200 text-black">
-                        {Object.keys(dayWorkouts).map(day => (
-                            <th key={day} className="border px-4 py-2">{day}</th>
+
+                <div className="mt-8 w-full max-w-2xl">
+                    <h3 className="text-2xl font-semibold mb-4 text-black text-center">Selected Exercises for {selectedDay}:</h3>
+                    {getSelectedWorkoutInputs()}
+                </div>
+
+                <div className="mt-8 w-full max-w-2xl text-center">
+                    <button 
+                        onClick={saveDayWorkout} 
+                        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
+                    >
+                        Save Workout Plan for {selectedDay}
+                    </button>
+                </div>
+
+                <div className="mt-8 w-full max-w-2xl text-center">
+                    <h3 className="text-2xl font-semibold mb-4 text-black">Select Day of the Week:</h3>
+                    <div className="flex justify-center space-x-4 text-black">
+                        {["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].map(day => (
+                            <label key={day} className="flex items-center">
+                                <input 
+                                    type="radio" 
+                                    id={day} 
+                                    name="dayOfWeek" 
+                                    value={day} 
+                                    className="mr-2" 
+                                    onChange={handleDayRadiobuttonChange} 
+                                    checked={selectedDay === day} 
+                                />
+                                {day}
+                            </label>
                         ))}
-                    </tr>
-                </thead>
-                <tbody className="text-black">{getDayWorkoutsTableBody()}</tbody>
-            </table>
+                    </div>
+                </div>
+
+                <div className="mt-8 w-full">
+                    <table className="table-auto w-full">
+                        <thead>
+                            <tr className="bg-gray-200 text-black">
+                                {Object.keys(dayWorkouts).map(day => (
+                                    <th key={day} className="border px-4 py-2">{day}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody className="text-black">{getDayWorkoutsTableBody()}</tbody>
+                    </table>
+                </div>
+            </main>
         </div>
     );
 }
+
--- pages/landing.js
+++ pages/landing.js
@@ -1,31 +1,29 @@
-import  LoginButton  from '../components/login_button';
 import Link from 'next/link';
+import LoginButton from '../components/login_button';
+import { Footer } from '@/components/footer';
 
 export default function Home() {
     return (
-        <div className="min-h-screen flex flex-col justify-center items-center bg-gray-900 text-white p-4">
+        <div className="min-h-screen flex flex-col bg-gray-900 text-white">
             {/* Header */}
-            <div className="w-full flex justify-between items-center p-4">
-                {/* Placeholder for potential logo or other elements */}
+            <header className="w-full flex justify-between items-center p-4 bg-gray-800 shadow-md fixed top-0 z-10">
                 <div className="flex items-center space-x-4">
                     {/* Logo placeholder */}
-                    <img src="IMG_20240517_235044_574.png" alt="Logo" className="w-12 h-12" />
+                    <img src="/IMG_20240517_235044_574.png" alt="Logo" className="w-12 h-12" />
                     {/* Page Title */}
                     <h1 className="text-2xl font-bold">DARK</h1>
                 </div>
                 
                 {/* Sign In Button */}
-
-                    <div className="text-sm bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded transition duration-300">
-                        <LoginButton/>
-                    </div>
-
-            </div>
+                <div className="text-sm bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded transition duration-300">
+                    <LoginButton />
+                </div>
+            </header>
 
             {/* Main Content */}
-            <div className="flex flex-col items-center">
-                {/* Logo placeholder */}
-                <img src="IMG_20240517_235044_574.png" alt="Logo" className="w-32 h-32 mb-6" />
+            <main className="flex flex-col items-center justify-center flex-grow mt-20 p-4">
+                {/* Logo Placeholder */}
+                <img src="/IMG_20240517_235044_574.png" alt="Logo" className="w-32 h-32 mb-6" />
                 
                 {/* Page Title */}
                 <h1 className="text-5xl font-bold mb-8">DARK</h1>
@@ -36,7 +34,8 @@ export default function Home() {
                         Go to Workout Generator
                     </button>
                 </Link>
-            </div>
+            </main>
         </div>
     );
 }
+
