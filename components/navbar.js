0a5fcc8dc958cdbec9f37f8244ab50d486f9a3ca
Carlo068 <carlo.ruiz.scch@gmail.com>
2024-08-03 10:44:31 -0600

    chore: Update navbar links to use Next.js Link component

--- components/navbar.js
+++ components/navbar.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import Link from 'next/link';
 
 export function Navbar() {
     return (
-        <nav className="bg-white shadow-md ">
-            <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize ">
-                <a href="/" className="border-b-2 border-transparent hover:text-gray-800  hover:border-blue-500 mx-1.5 sm:mx-6"> Home</a>
-
-                <a href="/feed" className="border-b-2 border-transparent hover:text-gray-800  hover:border-blue-500 mx-1.5 sm:mx-6">Feed</a>
-
-                <a href="/workout_generator" className="border-b-2 border-transparent hover:text-gray-800 hover:border-blue-500 mx-1.5 sm:mx-6">Create</a>
-
-                <a href="/myroutines" className="border-b-2 border-transparent hover:text-gray-800 hover:border-blue-500 mx-1.5 sm:mx-6">Routines</a>
+        <nav className="bg-white shadow-md">
+            <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize">
+                <Link href="/" passHref>
+                    <button className="border-b-2 border-transparent hover:text-gray-800 hover:border-blue-500 mx-1.5 sm:mx-6">Home</button>
+                </Link>
+                
+                <Link href="/feed" passHref>
+                    <button className="border-b-2 border-transparent hover:text-gray-800 hover:border-blue-500 mx-1.5 sm:mx-6">Feed</button>
+                </Link>
+                
+                <Link href="/workout_generator" passHref>
+                    <button className="border-b-2 border-transparent hover:text-gray-800 hover:border-blue-500 mx-1.5 sm:mx-6">Create</button>
+                </Link>
+                
+                <Link href="/myroutines" passHref>
+                    <button className="border-b-2 border-transparent hover:text-gray-800 hover:border-blue-500 mx-1.5 sm:mx-6">Routines</button>
+                </Link>
             </div>
         </nav>
     );
